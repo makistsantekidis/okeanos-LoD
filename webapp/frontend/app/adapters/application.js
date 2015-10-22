a92720e320237bad5bc117f3ed17257fa106d381
Avraam Tsantekidis <avraamt@lab.grid.auth.gr>
2015-10-22 17:39:22 +0300

    change-me

--- webapp/frontend/app/adapters/application.js
+++ webapp/frontend/app/adapters/application.js
@@ -2,7 +2,14 @@ import DS from "ember-data";
 import ENV from 'frontend/config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   host: ENV.host + ':80',
   namespace: 'api',
+  headers: {
+    'Accept': "application/json"
+  },
+  buildURL: function(type, id, record){
+    return this._super(type, id, record) + '/';
+  },
+  authorizer: 'authorizer:django'
  });
